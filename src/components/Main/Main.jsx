17ebc9fe1384aac12843c7191fa64db57f280bb5
Thetwin21 <sylvesterogodo@gmail.com>
2025-03-30 02:37:12 -0700

    final commit

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -5,9 +5,9 @@ import { Context } from '../../context/Context'
 
 const Main = () => {
     const {
-        onSent, 
-        recentPrompts, 
-        showResult, 
+        onSent,
+        recentPrompt,
+        showResult,
         resultData,
         loading,
         setInput,
@@ -20,43 +20,67 @@ const Main = () => {
                 <img src={assets.user_icon} alt="" />
             </div>
             <div className="main-container">
-                <div className="greet">
-                    <p><span>Hello, Sylvester.</span></p>
-                    <p>How can I help you today?</p>
-                </div>
-                <div className="cards">
-                    <div className="card">
-                        <p>
-                            Suggest Beautiful places to see on an upcoming road trip
-                        </p>
-                        <img src={assets.compass_icon} alt="" />
+                {!showResult ?
+                    <>
+                        <div className="greet">
+                            <p><span>Hello, Sylvester.</span></p>
+                            <p>How can I help you today?</p>
+                        </div>
+                        <div className="cards">
+                            <div className="card">
+                                <p>
+                                    Suggest Beautiful places to see on an upcoming road trip
+                                </p>
+                                <img src={assets.compass_icon} alt="" />
+                            </div>
+                            <div className="card">
+                                <p>
+                                    Briefly summarize this concept: urban planning
+                                </p>
+                                <img src={assets.bulb_icon} alt="" />
+                            </div>
+                            <div className="card">
+                                <p>
+                                    Brainstaorm team bonding activities for our work retreat
+                                </p>
+                                <img src={assets.message_icon} alt="" />
+                            </div>
+                            <div className="card">
+                                <p>
+                                    Improve the readability of following code
+                                </p>
+                                <img src={assets.code_icon} alt="" />
+                            </div>
+                        </div>
+                    </>
+                    :
+                    <div className='result'>
+                        <div className="result-title">
+                            <img src={assets.user_icon} alt="" />
+                            <p>{recentPrompt}</p>
+                        </div>
+                        <div className="result-data">
+                            <img src={assets.gemini_icon} alt="" />
+                            {
+                                loading ?
+                                    <div className="loader">
+                                        <hr />
+                                        <hr />
+                                        <hr />
+                                    </div>
+                                    :
+                                    <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
+                            }
+                        </div>
                     </div>
-                    <div className="card">
-                        <p>
-                            Briefly summarize this concept: urban planning
-                        </p>
-                        <img src={assets.bulb_icon} alt="" />
-                    </div>
-                    <div className="card">
-                        <p>
-                            Brainstaorm team bonding activities for our work retreat
-                        </p>
-                        <img src={assets.message_icon} alt="" />
-                    </div>
-                    <div className="card">
-                        <p>
-                            Improve the readability of following code
-                        </p>
-                        <img src={assets.code_icon} alt="" />
-                    </div>
-                </div>
+                }
                 <div className="main-bottom">
                     <div className="search-box">
-                        <input onChange={(e)=> setInput(e.target.value)} value={input} type="text" placeholder='Enter a prompt here' />
+                        <input onChange={(e) => setInput(e.target.value)} value={input} type="text" placeholder='Enter a prompt here' />
                         <div>
                             <img src={assets.gallery_icon} alt="" />
                             <img src={assets.mic_icon} alt="" />
-                            <img onClick={()=> onSent()} src={assets.send_icon} alt="" />
+                            {input? <img onClick={() => onSent()} src={assets.send_icon} alt="" /> : null}
                         </div>
                     </div>
                     <p className="bottom-info">
